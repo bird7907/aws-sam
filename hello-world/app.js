d6a8943fa6e609d269ac5c8bd9e9cd0ac9eb905d
bird7907 <bird790711>
2022-08-16 16:23:11 +0800

    change

--- hello-world/app.js
+++ hello-world/app.js
@@ -21,7 +21,7 @@ exports.lambdaHandler = async (event, context) => {
     response = {
       statusCode: 200,
       body: JSON.stringify({
-        message: "Hello world zhai",
+        message: "Hello world zhai-test",
         // location: ret.data.trim()
       }),
     };
